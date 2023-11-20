4d13300cbf9dfc204c3bf4c332f3f220a1b00c1d
David Onifade <davidicfola@gmail.com>
2023-11-20 19:21:46 +0000

    font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google'
+import { Archivo } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const archivo = Archivo({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Cloudnetvox',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={archivo.className}>{children}</body>
     </html>
   )
 }
