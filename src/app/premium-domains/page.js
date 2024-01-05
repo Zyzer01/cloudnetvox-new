0978974b72c5e9f40e7ff705a5b825747c72e236
David Onifade <davidicfola@gmail.com>
2024-01-05 09:42:53 +0000

    font issues

--- src/app/premium-domains/page.js
+++ src/app/premium-domains/page.js
@@ -1,8 +1,19 @@
+import AltHero from "@/components/AltHero";
 import PremiumDomainList from "@/components/PremiumDomainList";
+import Spacer from "@/components/Spacer";
 
 const PremiumDomains = () => {
   return (
+    <main>
+      <AltHero
+        pageTitle="Premium Domain Name"
+        pageSub="We'll make sure you find the right domain and that it's got a secure home online."
+        breadcrumb="Premium Domains"
+      />
+
       <PremiumDomainList />
+      <Spacer />
+    </main>
   );
 };
 
--- src/components/PremiumDomainList.jsx
+++ src/components/PremiumDomainList.jsx
@@ -1,160 +1,118 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
 import "../styles/table.css";
+import Header from "./Header";
+import { domains } from "@/utility/PremiumDomainNameData";
+import { motion, AnimatePresence } from "framer-motion";
+import { BsFire } from "react-icons/bs";
+import RegularCta from "./RegularCta";
+import ctaImageSrc0 from "public/images/girl sitting on domain animation.jpg";
 
-const PremiumDomainRow () => {
-  
-}
+
+const PremiumDomainRow = ({ cols }) => {
+  return (
+    <motion.tr
+      initial={{ opacity: 0, y: -10 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -10 }}
+    >
+      {cols.map((col, index) => (
+        <td key={index}>
+          <Link href="https://cloudnetvox.com/clients/cart.php?a=add&domain=register">
+            {col}
+          </Link>
+        </td>
+      ))}
+    </motion.tr>
+  );
+};
+
 const PremiumDomainList = () => {
+  const [visibleRow, setVisibleRow] = useState(5);
+  const [showMore, setShowMore] = useState(false);
+
+  const handleShowMore = () => {
+    setVisibleRow(domains.length);
+    setShowMore(true);
+  };
+
+  const handleHide = () => {
+    setVisibleRow(10);
+    setShowMore(false);
+  };
+
+  const columnsPerRow = 5;
+
+  const rows = domains.reduce((acc, domain, index) => {
+    const rowIndex = Math.floor(index / columnsPerRow);
+    acc[rowIndex] = acc[rowIndex] || [];
+    acc[rowIndex].push(domain);
+    return acc;
+  }, []);
+
+  const rippleRedVariants = {
+    initial: {
+      boxShadow:
+        "0 0 0 0 rgba(255, 0, 0, 0.1), 0 0 0 10px rgba(255, 0, 0, 0.1), 0 0 0 20px rgba(255, 0, 0, 0.1)",
+    },
+    animate: {
+      boxShadow:
+        "0 0 0 10px rgba(255, 0, 0, 0.1), 0 0 0 20px rgba(255, 0, 0, 0.1), 0 0 0 30px rgba(255, 0, 0, 0)",
+    },
+  };
+
   return (
     <div>
-      <div class="domain-table-section flex justify-center">
-        <div class="container ">
-          <div class="table-plan-box">
-            <table class="table">
+      {/* <Header heading="Premium Domain name" sub="i dont know what to add" /> */}
+      <RegularCta
+        heading="Elevate Your Online Presence With CloudNetvox!"
+        description="Stand out from the crowd with domain names that are memorable, relevant, and distinctly reflective of your brand. A premium domain instantly communicates professionalism and trustworthiness to potential customers, partners, and clients."
+        ctaImageSrc={ctaImageSrc0}
+        hideImage
+      />
+      <div className="domain-table-section bg-primary  text-domain flex justify-center">
+        <div className="container bg-white mx-16 rounded-2xl">
+          <div className="table-plan-box">
+            <table className="table">
               <thead>
                 <tr>
-                  <th colSpan="5">
-                    <h3>Premium Domain List</h3>
+                  <th colSpan={columnsPerRow}>
+                    <h3 className="flex">
+                      Premium Domain List
+                      <motion.span
+                        className="text-red-600 text-xl ml-5 rounded-full"
+                        initial="initial"
+                        animate="animate"
+                        variants={rippleRedVariants}
+                        transition={{ duration: 1, repeat: Infinity }}
+                      >
+                        <BsFire />
+                      </motion.span>
+                    </h3>
                   </th>
                 </tr>
               </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>net.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>box.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>dogfood.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>haircuts.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>loyal.ng</span>
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>abc.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>boy.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>doll.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>hallo.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>luck.ng</span>
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>abidi.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>bracelet.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>domainname.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>halloween.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>luna.ng</span>
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>ability.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>braces.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>domain.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>handbags.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>lunch.ng</span>
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>academia.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>brain.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>donwload.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>handguns.ng</span>
-                    </a>
-                  </td>
-                  <td>
-                    <a href="https://cloudnetvox.com/clients/cart.php?a=add&amp;domain=register">
-                      <span>lyric.ng</span>
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
+              <motion.tbody
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <AnimatePresence>
+                  {rows.slice(0, visibleRow).map((row, rowIndex) => (
+                    <PremiumDomainRow key={rowIndex} cols={row} />
+                  ))}
+                </AnimatePresence>
+              </motion.tbody>
             </table>
+            <div className="text-center mt-6 text-black">
+              <motion.button
+                whileTap={{ scale: 0.95 }}
+                onClick={showMore ? handleHide : handleShowMore}
+              >
+                {showMore ? "Hide" : "See more"}
+              </motion.button>
+            </div>
           </div>
         </div>
       </div>
