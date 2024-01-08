061bbc119aa2d0fe16c6d5b383e72f24fb68d493
David Onifade <davidicfola@gmail.com>
2024-01-08 23:38:51 +0000

    update AltHero.jsx update AltHero.jsx and HorizontalCta.jsx update HorizontalCta.jsx

--- src/components/AltHero.jsx
+++ src/components/AltHero.jsx
@@ -13,7 +13,7 @@ const AltHero = ({ pageTitle, pageSub, breadcrumb }) => {
           <BreadCrumbs page={breadcrumb} />
         </div> 
         <div></div>
-      </div>
+      </div> 
   );
 };
 
