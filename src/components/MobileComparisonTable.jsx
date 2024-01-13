a745561cbe9a103dca0656e8f06485d84c3e3bc7
David Onifade <davidicfola@gmail.com>
2024-01-13 01:41:13 +0000

    update MobileComparisonTable.jsx

--- src/components/MobileComparisonTable.jsx
+++ src/components/MobileComparisonTable.jsx
@@ -2,19 +2,51 @@
 import ButtonV2 from "./ui/ButtonV2";
 import Link from "next/link";
 import "../styles/table.css";
+import { useState } from "react";
+import { ImCheckmark } from "react-icons/im";
 
-const FeatureRow = ({ feature, lite, premium, extra }) => {
+const FeatureRow = ({ feature, lite, premium, extra, activeTab }) => {
   return (
     <tr className="">
       <td className="border-r">{feature}</td>
-      <td className="border-r text-center">{lite}</td>
-      <td className="border-r text-center">{premium}</td>
-      <td>{extra}</td>
+      {activeTab === "Lite" && <td className="border-r text-center">{lite}</td>}
+      {activeTab === "Premium" && (
+        <td className="border-r text-center">{premium}</td>
+      )}
+      {activeTab === "Extra" && <td>{extra}</td>}
     </tr>
   );
 };
 
-const MobileComparisonTable = ({ comparisonPlans, lite, premium, extra }) => {
+const MobileComparisonTable = ({ lite, premium, extra }) => {
+  const comparisonPlans = [
+    {
+      feature: "Visits per month",
+      lite: "Up to 10000",
+      premium: "Up to 50000",
+      extra: "Unlimited",
+    },
+    {
+      feature: "Free Domain Reg",
+      lite: <ImCheckmark />,
+      premium: <ImCheckmark />,
+      extra: <ImCheckmark />,
+    },
+    {
+      feature: "Visits per month",
+      lite: "Up to 10000",
+      premium: "Up to 50000",
+      extra: "Unlimited",
+    },
+  ];
+  const [activeTab, setActiveTab] = useState("Premium");
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  const options = ["Lite", "Premium", "Extra"];
+
   const buttonStyle = {
     padding: "8px 12px",
     backgroundColor: "white",
@@ -29,15 +61,23 @@ const MobileComparisonTable = ({ comparisonPlans, lite, premium, extra }) => {
   return (
     <>
       <div className="flex space-x-4 justify-center mb-8">
-        <button className="" style={buttonStyle}>
-          Lite
-        </button>
-        <button className="" style={buttonStyle}>
-          Premium
-        </button>
-        <button className="" style={buttonStyle}>
-          Extra
-        </button>
+        {options.map((tab) => (
+          <button
+            key={tab}
+            className={`py-2 px-3 border border-domain text-domain hover:drop-shadow-md transition ease-in-out duration-300 font-medium rounded-full ${
+              activeTab === tab
+                ? "bg-domain text-white font-medium hover:bg-domain"
+                : "md:hover:bg-domain md:hover:text-white"
+            }`}
+            onClick={() => handleTabClick(tab)}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            {tab}
+          </button>
+        ))}
       </div>
       <div className="flex justify-center p-8 md:p-28 bg-sky">
         <table className="table-fixed w-full border-collapse border-0 bg-white rounded-3xl pb-32">
@@ -45,62 +85,40 @@ const MobileComparisonTable = ({ comparisonPlans, lite, premium, extra }) => {
             <tr>
               <th className="border-0 invisible">Extensions (TLD)</th>
               <th className="border border-t-0">{lite.name}</th>
-              <th className="border border-t-0">{premium.name}</th>
+              <th className="border border-t-0">{premium.name}</th> 
               <th className="border border-t-0">{extra.name}</th>
             </tr>
           </thead>
           <tbody>
             <tr className="text-center">
               <td className="border-0 invisible">Extensions (TLD)</td>
-              <td className="border">
-                <div className="p-8">
-                  <p className="mb-2">Starting at:</p>
-                  <div>
-                    <h2 className="mb-3">
-                      ₦{lite.price}
-                      <span className="">/mo</span>
-                    </h2>
-                  </div>
-                  <div className="">
-                    <Link href="/new">
-                      <ButtonV2 text="Order now" />
-                    </Link>
-                  </div>
-                </div>
-              </td>
-              <td className="default border">
-                <div className="p-8">
-                  <p className="mb-2">Starting at:</p>
-                  <div>
-                    <h2 className="mb-3">
-                      ₦{premium.price}
-                      <span className="">/mo</span>
-                    </h2>
-                  </div>
-                  <div className="">
-                    <Link href="/new">
-                      <ButtonV2 text="Order now" />
-                    </Link>
-                  </div>
-                </div>
-              </td>
-              <td className="border">
-                <div className="p-8">
-                  <p className="mb-2">Starting at:</p>
-                  <div>
-                    <h2 className="mb-3">
-                      ₦{extra.price}
-                      <span className="">/mo</span>
-                    </h2>
-                  </div>
-                  <div className="">
-                    <Link href="/new">
-                      <ButtonV2 text="Order now" />
-                    </Link>
-                  </div>
-                </div>
-              </td>
+              {options.map((tab) => (
+                <td key={tab} className="border">
+                  {activeTab === tab && (
+                    <div className="p-8">
+                      <p className="mb-2">Starting at:</p>
+                      <div>
+                        <h2 className="mb-3">
+                          ₦
+                          {tab === "Lite"
+                            ? lite.price
+                            : tab === "Premium"
+                            ? premium.price
+                            : extra.price}
+                          <span className="">/mo</span>
+                        </h2>
+                      </div>
+                      <div className="">
+                        <Link href="/new">
+                          <ButtonV2 text="Order now" />
+                        </Link>
+                      </div>
+                    </div>
+                  )}
+                </td>
+              ))}
             </tr>
+
             {comparisonPlans.map((item, index) => (
               <FeatureRow
                 key={index}
@@ -108,6 +126,7 @@ const MobileComparisonTable = ({ comparisonPlans, lite, premium, extra }) => {
                 lite={item.lite}
                 premium={item.premium}
                 extra={item.extra}
+                activeTab={activeTab}
               />
             ))}
           </tbody>
