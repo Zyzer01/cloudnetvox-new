a6e1ed021f3a9b13191346e1c3e6ade5d0788dcd
David Onifade <davidicfola@gmail.com>
2024-01-15 22:45:59 +0000

    update HostAdviceWidget.jsx

--- src/components/HostAdviceWidget.jsx
+++ src/components/HostAdviceWidget.jsx
@@ -1,9 +1,6 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect } from 'react';
 
 const HostAdviceWidget = () => {
-  const [isWidgetLoaded, setWidgetLoaded] = useState(false);
-
   useEffect(() => {
     // Function to embed the HostAdvice script
     const embedHostAdviceScript = () => {
@@ -16,66 +13,18 @@ const HostAdviceWidget = () => {
       firstScript.parentNode.insertBefore(script, firstScript);
     };
 
-    // Embed HostAdvice script
     embedHostAdviceScript();
 
-    // Cleanup function to remove the script when the component is unmounted
     return () => {
       const scriptElement = document.getElementById('ha-embed');
       if (scriptElement) {
         scriptElement.parentNode.removeChild(scriptElement);
       }
     };
-  }, []); // Empty dependency array ensures this effect runs only once on mount
+  }, []); 
 
   return (
     <div className="ha-widget px-8 md:px-24 pt-8" data-widget="latest-reviews" data-height="500" data-width="auto" data-id="94020" data-lang="en" data-params="above_stars:"></div>
-  );
-};
-
-export default HostAdviceWidget;
-
-
-
-const HostAdviceWidget = () => {
-  const [isWidgetLoaded, setWidgetLoaded] = useState(false);
-
-  useEffect(() => {
-    // Function to embed the HostAdvice script
-    const embedHostAdviceScript = () => {
-      const script = document.createElement('script');
-      script.id = 'ha-embed';
-      script.async = true;
-      script.src = 'https://hostadvice.com/embed/widgets.js';
-
-      script.onload = () => {
-        // Set the loading state to false when the script is loaded
-        setWidgetLoaded(true);
-      };
-
-      const firstScript = document.getElementsByTagName('script')[0];
-      firstScript.parentNode.insertBefore(script, firstScript);
-    };
-
-    // Embed HostAdvice script
-    embedHostAdviceScript();
-
-    // Cleanup function to remove the script when the component is unmounted
-    return () => {
-      const scriptElement = document.getElementById('ha-embed');
-      if (scriptElement) {
-        scriptElement.parentNode.removeChild(scriptElement);
-      }
-    };
-  }, []); // Empty dependency array ensures this effect runs only once on mount
-
-  return (
-    <div>
-      {!isWidgetLoaded && <p className='text-center text-domain'>Loading...</p>}
-      {isWidgetLoaded && (
-        <div className="ha-widget px-8 md:px-24 pt-8" data-widget="latest-reviews" data-height="500" data-width="auto" data-id="94020" data-lang="en" data-params="above_stars:"></div>
-      )}
-    </div>
   );
 };
 
