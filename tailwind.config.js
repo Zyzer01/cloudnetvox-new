dec92b3546299de11b4e22e08a04e9045892b51e
David Onifade <davidicfola@gmail.com>
2024-01-15 22:24:17 +0000

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         '3xl': '0 8px 8px -1px rgba(0, 0, 0, 0.15);',
         spread: '0 30px 30px rgba(24, 27, 34, 0.10)',
       },
-      colors: {
+      colors: { 
         primary: '#06155A',
         domain: '#103dbe',
         slight: '#123072',
