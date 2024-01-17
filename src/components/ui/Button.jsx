cb143d54a1e43a0f0bd5a4beb51a0a229e090efd
David Onifade <davidicfola@gmail.com>
2024-01-17 22:32:46 +0000

    update Button.jsx

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 export default function Button({ text }) {
   return (
     <>
-      <motion.button
+      <motion.button 
         whileTap={{ scale: 0.95 }}
         className="py-3 px-8 bg-white text-domain sm:hover:text-white sm:hover:bg-domain hover:drop-shadow-md transition ease-in-out duration-300 font-medium rounded-full">
         {text}
