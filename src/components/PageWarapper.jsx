13bcabe531797893fc2b2536efbf74e52ce4fbaf
David Onifade <davidicfola@gmail.com>
2024-01-20 01:50:44 +0000

    delete 1 file and update 1 file

--- src/components/PageWarapper.jsx
+++ src/components/PageWarapper.jsx
@@ -5,11 +5,12 @@ const PageWarapper = ({ children }) => {
   return (
     <AnimatePresence mode="wait">
       <div
-        key={typeof window !== 'undefined' ? window.location.pathname : ''}
-        initial={{ opacity: 0, x: '100vw' }}
-        animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: '-100vw' }}
-        transition={{ duration: 0.3 }}>
+        // key={typeof window !== 'undefined' ? window.location.pathname : ''}
+        // initial={{ opacity: 0, x: '100vw' }}
+        // animate={{ opacity: 1, x: 0 }}
+        // exit={{ opacity: 0, x: '-100vw' }}
+        // transition={{ duration: 0.3 }}
+        >
         {children}
       </div>
     </AnimatePresence>
